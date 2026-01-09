6a5a03b2e57591d4d16f48230054aafc29023f80
Eleazer Tamatey Okoto <101367394+eleasel@users.noreply.github.com>
2026-01-09 10:47:43 +0000

    Update LogoBanner.tsx

--- app/components/LogoBanner.tsx
+++ app/components/LogoBanner.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image';
+
 export function LogoBanner() {
   return (
     <div className="absolute top-6 left-6 z-50">
-      <img 
+      <Image 
         src="/dti-logo.png" 
         alt="DTI Logo" 
-        className="h-16 w-16 object-contain opacity-90 hover:opacity-100 transition-opacity"
+        width={64}
+        height={64}
+        className="object-contain opacity-90 hover:opacity-100 transition-opacity"
       />
     </div>
   );
