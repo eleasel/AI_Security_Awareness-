8e2fe5e6a96d27a26f12c6d630354802b1c94ae9
Eleazer Tamatey Okoto <101367394+eleasel@users.noreply.github.com>
2026-01-09 10:30:42 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1315,16 +1315,3 @@ export default function Home() {
   );
 }
 
-File 2: /src/app/components/LogoBanner.tsx
-
-export function LogoBanner() {
-  return (
-    <div className="absolute top-6 left-6 z-50">
-      <img 
-        src={dtiLogo} 
-        alt="DTI Logo" 
-        className="h-16 w-16 object-contain opacity-90 hover:opacity-100 transition-opacity"
-      />
-    </div>
-  );
-}
